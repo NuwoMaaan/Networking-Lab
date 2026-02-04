c561bc929b93637dff9fd343f9fc78c4fea000b7
NuwoMaaan <Leoholden24@outlook.com>
2026-02-04 20:56:55 +1100

    naming fix

--- NAPALM/main.py
+++ NAPALM/main.py
@@ -12,7 +12,6 @@ def load_devices_attr(file_path: str) -> dict[str, dict]:
 def initialize_devices(device_attr: dict) -> list[DeviceConfig]:
     device_configs = []
     for device in device_attr:
-        # print(device_attr[device]["os"])
         device_config = DeviceConfig(
                 node_name = device,
                 OperatingSystem = device_attr[device]["os"],
@@ -24,37 +23,31 @@ def initialize_devices(device_attr: dict) -> list[DeviceConfig]:
         device_configs.append(device_config)
     return device_configs
 
-def initialize_drivers(devices: list) -> dict[str, NetworkDriver]:
-    drivers = {}
+def initialize_device_connections(devices: list[DeviceConfig]) -> dict[str, NetworkDriver]:
+    connections = {}
     for device in devices:
         driver = napalm.get_network_driver(device.OperatingSystem)
-        drivers[device.node_name] = driver(
+        connections[device.node_name] = driver(
                 hostname = device.hostname, 
                 username = device.username, 
                 password = device.password,
                 optional_args = {"secret": device.secret},
                 timeout=60
         )
-    #print(drivers[device.node_name].hostname)
-    return drivers
+    return connections
 
-def connect_to_devices(drivers: dict[str, NetworkDriver]) -> str:
+def connect_to_devices(devices: dict[str, NetworkDriver]) -> None:
     """Get facts test"""
-    for d in drivers:
-        with drivers[d] as device:
+    for d in devices:
+        with devices[d] as device:
             print(device.get_facts())
 
 
 def main():
     device_attr = load_devices_attr("devices.yaml")
-    devices = initialize_devices(device_attr["devices"])
-    drivers = initialize_drivers(devices)
-    connect_to_devices(drivers)
-    
-    # with driver(**DSW1_settings, optional_args={"secret": "password",'read_timeout': 120}) as device:
-    #     output.append(device.get_facts())
-    #     for i in output:
-    #         print(json.dumps(i, indent=4))
+    device_configs = initialize_devices(device_attr["devices"])
+    devices = initialize_device_connections(device_configs)
+    connect_to_devices(devices)
 
 if __name__ == "__main__":
     main()
